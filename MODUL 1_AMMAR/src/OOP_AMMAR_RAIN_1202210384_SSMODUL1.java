4a8d79fdb77df502e8ac8690af2012e35789dd03
jundana110503 <91583533+jundana110503@users.noreply.github.com>
2022-10-31 08:58:20 +0700

    Add files via upload

---
+++ MODUL 1_AMMAR/src/OOP_AMMAR_RAIN_1202210384_SSMODUL1.java
@@ -0,0 +1,13 @@
+class menunya {
+    public static void main(String[] args){
+        System.out.println ("Selamat Datang Di Restoran EAD");    
+        System.out.println ("Silahkan Register Terlebih Dulu");
+        System.out.println ("===============================");
+        System.out.println ("Nama: Ammar_SI4505");
+        System.out.println ("No Hanphone: 08713626542");
+}
+ String name;
+ String category;
+ String price;
+}
+   
